3f2b2ce61fbfc1a8d692077cd8f1022293dfce42
florianthepro <florianthepro@icloud.com>
2026-08-27 14:14:26 +0200

    Update caddy.bat

--- caddy.bat
+++ caddy.bat
@@ -1,13 +1,8 @@
 @echo off
 setlocal
-
 if not exist "C:\caddy\" mkdir "C:\caddy"
 if not exist "C:\caddy\caddy.exe" powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://caddyserver.com/api/download?os=windows&arch=amd64' -OutFile 'C:\caddy\caddy.exe'"
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/florianthepro/caddy/main/readme' -OutFile 'C:\caddy\caddyfile'"
-
+if not exist "C:\caddy\caddyfile" powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/florianthepro/caddy/main/readme' -OutFile 'C:\caddy\caddyfile'"
 schtasks /create /tn "caddy start" /sc onstart /ru SYSTEM /rl HIGHEST /tr "C:\caddy\caddy.exe run --config C:\caddy\caddyfile" /f
-
 tasklist /FI "IMAGENAME eq caddy.exe" | find /I "caddy.exe" >nul || schtasks /run /tn "caddy start"
-
 endlocal
