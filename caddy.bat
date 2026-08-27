a479bb0632b0503ce9e32ba3742cd7c611d3251b
florianthepro <florianthepro@icloud.com>
2026-08-27 12:38:37 +0200

    Update caddy.bat

--- caddy.bat
+++ caddy.bat
@@ -7,7 +7,7 @@ if not exist "C:\caddy\caddy.exe" echo C:\caddy\caddy.exe missing && exit /b
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/florianthepro/caddy/main/caddyfile' -OutFile 'C:\caddy\caddyfile'"
 
 schtasks /create /tn "caddy start" /sc onstart /ru SYSTEM /rl HIGHEST /tr "C:\caddy\caddy.exe run --config C:\caddy\caddyfile" /f
-schtasks /end /tn "caddy start"
-schtasks /run /tn "caddy start"
+
+tasklist /FI "IMAGENAME eq caddy.exe" | find /I "caddy.exe" >nul || schtasks /run /tn "caddy start"
 
 endlocal
